c962ec1b00dc04ab688a295765ecc57fd3be7543
Kevin Moore <kevin@thinkpixellab.com>
2010-05-09 20:25:21 -0700

    Really working...

--- tree.rb
+++ tree.rb
@@ -2,30 +2,56 @@ require "rubygems"
 require "bundler"
 require "graphviz"
 
-
 ENV['BUNDLE_GEMFILE'] = '/Users/kevin/tpl/Gemfile'
 
-all_specs = Bundler.runtime.specs.to_a
+graph = GraphViz::new('Gemfile')
+graph.node[:fontname => 'Helvetica']
 
-
-g = GraphViz::new( "G" )
+clusters = {}
 
 nodes = {}
+# add all specs to the graph
+# flag all spacs there were explicitly required
+Bundler.definition.dependencies.each do |d|
+  groups = d.groups.reject{ |g| g == :default}
 
-all_specs.each do |s|
-  nodes[s.name] = g.add_node(s.name)
+  parent = graph
+  if(groups.size == 1)
+    group = groups[0]
+
+    if clusters.has_key?(group)
+      parent = clusters[group]
+    else
+      clusters[group] = parent = graph.add_graph(
+        "cluster_#{group}",
+        {:label => group}
+      )
+    end
+  end
+
+  nodes[d.name] = parent.add_node(d.name, {:style => 'bold',
+
+    :fontname => 'Helvetica-Bold',
+    :style => 'filled',
+    :fillcolor => '#aaaacc'})
 end
 
-all_specs.each do |s|
-  
+Bundler.runtime.specs.each do |s|
+  if !nodes.has_key? s.name
+    nodes[s.name] = graph.add_node(s.name)
+  end
+end
+
+
+Bundler.runtime.specs.each do |s|
+
   from = nodes[s.name]
-  
+
   s.runtime_dependencies.each do |d|
     to = nodes[d.name]
-    g.add_edge( from, to )
-    
+    from << to
   end
-  
+
 end
 
-g.output( :png => "test.png" )
+graph.output( :svg => "test.svg" )
