36d88a3221e5922be3eb792ff5703899527a4d8f
Kevin Moore <kevin@thinkpixellab.com>
2010-05-09 21:10:52 -0700

    A bunch of comments

--- tree.rb
+++ tree.rb
@@ -2,20 +2,33 @@ require "rubygems"
 require "bundler"
 require "graphviz"
 
-ENV['BUNDLE_GEMFILE'] = '/Users/kevin/tpl/Gemfile'
+# ENV['BUNDLE_GEMFILE'] = "set to a Gemfile path if you don't run this in a directory with a Gemfile"
 
 graph = GraphViz::new('Gemfile')
 graph.graph[:fontname] = graph.node[:fontname] = 'Helvetica'
 
+# clusters store sub-graphs that aren't default. 'test' is common, for instance.
 clusters = {}
 
+# hash of name => graph node
 nodes = {}
-# add all specs to the graph
-# flag all spacs there were explicitly required
+
+# Walk through the dependencies.
+#  This is done before walking the specs, since we can know
+#  1) This was an explicit requirement by the user
+#  2) What group (if any) the item is associated with
 Bundler.definition.dependencies.each do |d|
+
+  # by default, all nodes are just children of the main graph
+  parent = graph
+
+  # But if a dependency is in a non-default group, we want to
+  #  cluster them seperately
   groups = d.groups.reject{ |g| g == :default}
 
-  parent = graph
+  # graphviz doesn't support overlapping clusters.
+  # A node can't be in both cluster 'a' and 'b'
+  # So only cluster a node iff it's in only one non-default group
   if(groups.size == 1)
     group = groups[0]
 
@@ -29,6 +42,8 @@ Bundler.definition.dependencies.each do |d|
     end
   end
 
+  # Add a node (with styles for a dependency) to the graph
+  # and save it in our hash
   nodes[d.name] = parent.add_node(d.name, {:style => 'bold',
     :fontname => 'Helvetica-Bold',
     :style => 'filled',
@@ -37,23 +52,27 @@ Bundler.definition.dependencies.each do |d|
 
 end
 
+# Now walk through all of the 'specs'
 Bundler.runtime.specs.each do |spec|
   name = spec.name
   node = nil
+
   if nodes.has_key? name
+    # we might have already added this one
     node = nodes[name]
   else
+    # If we haven't create it
     node = nodes[name] = graph.add_node(name)
   end
 
+  # display all system gems in boxes
   node[:shape] = 'box' if spec.source.to_s == 'system gems'
 end
 
-
+# now walk though a final time and add edges
 Bundler.runtime.specs.each do |s|
 
   from = nodes[s.name]
-
   s.runtime_dependencies.each do |d|
     to = nodes[d.name]
     from << to
@@ -61,5 +80,5 @@ Bundler.runtime.specs.each do |s|
 
 end
 
-graph.output( :png => "test.png" )
-`open test.png`
+# You can do :svg => "file.svg" here, etc.
+graph.output( :png => "Gemfile.png" )
